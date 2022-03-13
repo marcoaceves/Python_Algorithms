efe4340f4287f8af64bb18a2a4cb0367f3fea835
MarcoAceves <mr.aceves@gmail.com>
2022-03-13 16:10:18 -0700

    strings to do

--- strings_to_do_1/strings_to_do_1.py
+++ strings_to_do_1/strings_to_do_1.py
@@ -7,9 +7,11 @@ def remove_blanks(string):
     remove=' '
 
     for i in remove:
-	    string = string.replace(i, "")
+        string = string.replace(i, "")
 
     print(string)
+
+
 
 remove_blanks(string1)
 
@@ -69,3 +71,50 @@ print(acronym(" there's no free lunch - gotta pay yer way. "))
 # Zip Arrays into Map
 
 # Associative arrays are sometimes called maps because a key (string) maps to a value. Given two arrays, create an associative array (map) containing keys of the first, and values of the second. For arr1 = ["abc", 3, "yo"] and arr2 = [42, "wassup", true], return {"abc": 42, 3: "wassup", "yo": true}.
+
+arr1 = ["abc", 3, "yo"]
+arr2 = [42, "wassup", True]
+
+
+def zipmap(arr1, arr2):
+    newnew={}
+    if len(arr1) >= len(arr2):
+        for i in range(len(arr1)):
+            newnew[arr1[i]] = (arr2[i])
+    else:
+        for i in range(len(arr2)):
+            newnew[arr1[i]] = (arr2[i])
+
+
+    return newnew
+
+print(zipmap(arr1,arr2))
+
+
+# Invert Hash
+
+# Associative arrays are also called hashes (we’ll learn why later). Build invertHash(assocArr) to convert hash keys to values, and values to keys.
+
+# Example: given {"name": "Zaphod", "charm": "high", "morals": "dicey"}, return object {"Zaphod": "name", "high":"charm", "dicey": "morals"}.
+
+hash_array= {"name": "Zaphod", "charm": "high", "morals": "dicey"}
+
+def invert_hash(arr):
+    keys=[]
+    values=[]
+
+    new_dict={}
+    for key in arr:
+        keys.append(key)
+
+    for key, value in arr.items():
+        values.append(value)
+
+
+    for i in range(len(keys)):
+        new_dict[values[i]] = (keys[i])
+
+    # print(keys, values)
+
+    print(new_dict)
+invert_hash(hash_array)
