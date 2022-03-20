d8527e3615e677647a6cb330cb6e9f2d67362481
MarcoAceves <mr.aceves@gmail.com>
2022-03-20 11:38:42 -0700

    movind, and deleting nodes

--- building_a_linked_list/linked_list.py
+++ building_a_linked_list/linked_list.py
@@ -3,7 +3,7 @@ from hashlib import new
 
 class Node():
     def __init__ (self, value):
-        self. value = value
+        self.value = value
         self.next = None
 
 class SLL():
@@ -22,6 +22,7 @@ class SLL():
             runner = runner.next
         return self
 
+
     # add to front
     def add_front(self, value):
         temp= self.head
@@ -33,11 +34,13 @@ class SLL():
     def display(self):
         runner = self.head
         count=1
+        list1=[]
         while(runner):
-            print(f'This is your {count} node, it contains value {runner.value}')
+            # print(f'This is your {count} node, it contains value {runner.value}')
+            list1.append(runner.value)
             count += 1
             runner = runner.next
-        return self
+        print(list1)
 
     # display number of nodes
     def number_of_nodes(self):
@@ -60,45 +63,75 @@ class SLL():
             self.head = self.head.next
             temp=None
 
+    # if contains value delete
+    def contains_delete(self, val):
 
+        runner = self.head
+        while(runner):
+            if runner.next:
+                if runner.next.value == val:
+                    temp = runner.next
+                    runner.next = temp.next
+            runner = runner.next
+        return self
 
     # move min to front
     def min_to_front(self):
         runner = self.head
-        temp= runner.value
-        node=temp
-        runner2 = runner.next
         count=0
+        temp = runner.value
         while(runner):
-            if runner.value < runner2.value:
-                count+=1
-                temp=runner.value
-                node=runner
-                print(temp, 'is your new lowest value', ', node:',count)
-            else:
-
-                count+=1
-                print(runner2.value, 'is NOT your lowest value', ', node:',count)
+            if runner.next:
+                if temp > runner.next.value :
+                    count+=1
+                    temp=runner.next.value
+                    print('temp',temp)
+                else:
+                    count+=1
             runner = runner.next
-
-        # while(runner):
-        #     if runner.next == node:
-        #         runner.value== runner.next
-        # runner = runner.next
-        new_sll.add_front(temp)
+        print(temp, 'is your lowest value')
+        self.contains_delete(temp)
+        self.add_front(temp)
 
         print('')
 
+    def max_to_back(self):
+        runner = self.head
+        max1= runner.value
+        while(runner):
+            if  (max1 < runner.value):
+                max1 = runner.value
 
-new_sll = SLL(Node(1))
-new_sll.add_front(2).add_front(4).add_front(22).add_front(43).add_front(7)
-new_sll.display()
-new_sll.front_value()
+
+            runner = runner.next
+        self.contains_delete(max1)
+        self.push_back(max1)
+        print(max1,'is max value')
+
+    def push_back(self, newElement):
+        newNode = Node(newElement)
+        if(self.head == None):
+            self.head = newNode
+            return
+        else:
+            temp = self.head
+            while(temp.next != None):
+                temp = temp.next
+            temp.next = newNode
+
+
+new_sll = SLL(Node(2))
+new_sll.add_front(1).add_front(4).add_front(22).add_front(43).add_front(7)
+# new_sll.display()
+# new_sll.front_value()
 # new_sll.delete_front()
 new_sll.display()
-new_sll.contains(7)
-new_sll.number_of_nodes()
-new_sll.min_to_front()
+# new_sll.contains(7)
+# new_sll.number_of_nodes()
+# new_sll.min_to_front()
+# new_sll.display()
+new_sll.max_to_back()
+
 new_sll.display()
 
 
