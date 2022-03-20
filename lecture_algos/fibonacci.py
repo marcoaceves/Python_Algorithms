c02943470cf729cbf1961b98df6cce1a3653fd7c
MarcoAceves <mr.aceves@gmail.com>
2022-03-19 18:38:16 -0700

    2 ways

--- lecture_algos/fibonacci.py
+++ lecture_algos/fibonacci.py
@@ -1,16 +1,27 @@
 
 
+# def fibonacci(num):
+
+#     num1=0
+#     num2=1
+
+
+#     if num <= 0:
+#         print('Not going to work')
+#     for i in range(2, num):
+#         num3 = num1 + num2
+#         num1 = num2
+#         num2 = num3
+#         print(num2)
+
+
+
+
+# fibonacci(3)
+
 def fibonacci(num):
-
-    num1=0
-    num2=1
-    for i in range(2, num):
-        num3 = num1 + num2
-        num1 = num2
-        num2 = num3
-        print(num2)
-
-
-
-
-fibonacci(17)
+    if num <= 1:
+        return(num)
+    else:
+        return fibonacci(num - 1) + fibonacci(num-2)
+print(fibonacci(7))
