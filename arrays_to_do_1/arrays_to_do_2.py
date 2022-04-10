99a7be96062e95a58991a9558eff74f1d306c37b
MarcoAceves <mr.aceves@gmail.com>
2022-04-09 20:45:23 -0700

    find and remove max and min , rotate array

--- arrays_to_do_1/arrays_to_do_2.py
+++ arrays_to_do_1/arrays_to_do_2.py
@@ -1,6 +1,6 @@
 # Given a numerical array, reverse the order of values, in-place. The reversed array should have the same length, with existing elements moved to other indices so that order of elements is reversed. Working 'in-place' means that you cannot use a second 
 # array – move values within the array that you are given. As always, do not use built-in array functions such as splice().
-arr=[1,2,3,4,5,1,9]
+arr=[1,2,3,4,5,6]
 
 def reverse(arr):
     x=1
@@ -12,3 +12,59 @@ def reverse(arr):
     return arr
 
 print(reverse(arr))
+
+
+# Implement rotateArr(arr, shiftBy) that accepts array and offset. Shift arr's values to the right by that amount. 'Wrap-around' any values that shift off array's end to the other side, so that no data is lost. Operate in-place: given ([1,2,3],1), 
+# change the array to [3,1,2]. Don't use built-in functions.
+# Second: allow negative shiftBy (shift L, not R).
+# Third: minimize memory usage. With no new array, handle arrays/shiftBys in the millions.
+# Fourth: minimize the touches of each element.
+
+def rotate_arr(arr, shiftby):
+
+    for i in range(shiftby):
+
+        last=arr[len(arr)-1]
+
+        for j in range(len(arr)-1,-1,-1):
+            arr[j] = arr[j-1]
+
+        arr[0]=last
+    print(arr)
+rotate_arr(arr,1)
+
+def rotate_arr_left(arr, shiftby):
+
+    for i in range(shiftby):
+
+        first=arr[0]
+
+        for j in range(len(arr)-1):
+            arr[j] = arr[j+1]
+
+        arr[len(arr)-1]=first
+    print(arr)
+
+rotate_arr_left(arr,1)
+
+# Alan is good at breaking secret codes. One method is to eliminate values that lie outside of a specific known range. Given arr and values min and max, retain only the array values between min and max. Work in-place: return the array you are given, with values in original order. No built-in array functions.
+
+def elim_min_max(arr):
+    min=arr[0]
+    max= arr[0]
+
+    for i in range(len(arr)-1):
+        if arr[i]< arr[i+1]:
+            max=arr[i+1]
+        if arr[i]> arr[i+1]:
+            min=arr[i+1]
+    print(min,max)
+    for x in range(len(arr)-1):
+        if arr[x]== min:
+            arr.pop(x)
+    for x in range(len(arr)-1):
+        if arr[x]== max:
+            arr.pop(x)
+
+    print(arr)
+elim_min_max(arr)
