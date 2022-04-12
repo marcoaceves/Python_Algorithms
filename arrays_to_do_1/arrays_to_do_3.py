9ce8480492fac87d5bc1be124f78d18b41b7177a
Marco Aceves <85464208+marcoaceves@users.noreply.github.com>
2022-04-11 22:12:50 -0700

    arrays

--- arrays_to_do_1/arrays_to_do_3.py
+++ arrays_to_do_1/arrays_to_do_3.py
@@ -1,3 +1,6 @@
+import math
+from traceback import print_tb
+
 # Implement removeNegatives() that accepts an array, removes negative values, and returns the same array (not a copy), preserving non-negatives’ order. As always, do not use built-in array functions.
 
 def removeNegatives(arr):
@@ -25,4 +28,61 @@ print(second_to_last([42,True,4,"Kate",7]))
 
 
 
+# Second-Largest
 
+# Return the second-largest element of an array. Given [42,1,4,Math.PI,7], return 7. If the array is too short, return null.
+
+
+def second_largest(arr):
+    largest=arr[0]
+    if len(arr) <= 1:
+        return 'null'
+    else:
+        for i in arr:
+            if i > largest:
+                largest = i
+        for i in arr:
+            if i < largest:
+                secondLargest= i
+                if i> secondLargest:
+                    secondLargest = i 
+    return(secondLargest)
+
+print(second_to_last([42]))
+print(second_largest([42,1,4,math.pi,7]))
+
+
+# Nth-to-Last
+
+# Return the element that is N-from-array’s-end. Given ([5,2,3,6,4,9,7],3), return 4. If the array is too short, return null.
+
+def n_to_last(arr,n):
+
+    if len(arr)-n < 0:
+        return 'null'
+    return (arr[len(arr)-n])
+
+print(n_to_last([5,2,3,6,4,9,7],3))
+
+
+
+# Nth-Largest
+
+# Liam has "N" number of Green Belt stickers for excellent Python projects. Given arr and N, return the Nth-largest element, where (N-1) elements are larger. Return null if needed.
+
+def Nth_largest(arr,n):
+    # i= sorted(arr)
+    for k in range(len(arr)-1):
+        for j in range(k+1, len(arr)):
+            if(arr[k] > arr[j]):
+                temp = arr[k];
+                arr[k] = arr[j];
+                arr[j] = temp;
+
+
+    return arr[len(arr)-n]
+
+
+
+
+print(Nth_largest([5,2,3,6,4,9,7],3))
