56d34b64c427b4731312c6a1dd96c3972e0fce98
Marco Aceves <85464208+marcoaceves@users.noreply.github.com>
2022-04-13 12:52:47 -0700

    skyline hieghts arrays

--- arrays_to_do_1/arrays_to_do_3.py
+++ arrays_to_do_1/arrays_to_do_3.py
@@ -86,3 +86,22 @@ def Nth_largest(arr,n):
 
 
 print(Nth_largest([5,2,3,6,4,9,7],3))
+
+
+# Skyline Heights
+
+# Lovely Burbank has a breathtaking view of the Los Angeles skyline. Let’s say you are given an array with heights of consecutive buildings, starting closest to you and extending away. Array [-1,7,3] would represent three buildings: first is actually out of view below street level, behind it is second at 7 stories high, third is 3 stories high (hidden behind the 7-story). You are situated at street level. Return array containing heights of buildings you can see, in order. Given [-1,1,1,7,3] return [1,7]. Given [0,4] return [4]. As always with challenges, do not use built-in array functions such as unshift().
+
+def skyline(arr):
+    new_arr=[]
+    temp=arr[0]
+    if arr[0]>0:
+            new_arr.append(arr[0])
+    for i in range(len(arr)-1):
+        if arr[i] < arr[i+1]:
+            new_arr.append(arr[i+1])
+
+    return new_arr
+
+
+print(skyline([-1,2,1,7,3]))
